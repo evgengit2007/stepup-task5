7dc2101b9bfdfc016146c05b44a963181ae627c6
evgengit2007 <vend111@yandex.ru>
2024-05-22 11:36:35 +0700

    first commit

---
+++ src/main/java/ru/stepup/course2/stepuptask5/StepupTask5Application.java
@@ -0,0 +1,13 @@
+package ru.stepup.course2.stepuptask5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StepupTask5Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StepupTask5Application.class, args);
+	}
+
+}
---
+++ src/test/java/ru/stepup/course2/stepuptask5/StepupTask5ApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.stepup.course2.stepuptask5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StepupTask5ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
