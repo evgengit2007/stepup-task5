48fbfbc671e22261c75a80489de75ad16fca7305
evgengit2007 <vend111@yandex.ru>
2024-05-25 13:13:54 +0700

    make package 2

--- src/main/java/ru/stepup/course2/stepuptask5/controller/AgreementController.java
+++ src/main/java/ru/stepup/course2/stepuptask5/controller/AgreementController.java
@@ -1,12 +1,18 @@
 package ru.stepup.course2.stepuptask5.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import ru.stepup.course2.stepuptask5.models.prod_register.ProdRegExample;
+
+import javax.print.attribute.standard.Media;
 
 @RestController
-@RequestMapping("api/v1")
 @RequiredArgsConstructor
+@RequestMapping(value = "/", produces = MediaType.APPLICATION_JSON_VALUE)
 public class AgreementController {
-
+    @Autowired
+    ProdRegExample prodRegExample;
 }
